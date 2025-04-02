07365c9f5d4bacc74c564f7f7af110670860ee50
raiyanmook27 <raiyan.mook27@gmail.com>
2025-04-02 22:58:35 +0000

    chore: Rust -  Primitive

---
+++ src/primitives/primitivesTest.rs
@@ -0,0 +1,27 @@
+fn main() {
+
+    //Primitive types
+    //scaler and compound
+
+    //Scaler
+    let mut x = -5i64;
+
+    x = 10i64;
+    let y: u32 = 10;
+    let f: f32 = 22.5;
+    //let mut z = 67.9f64;
+    //let mut mutable = 12;
+
+    // let mut mutable = 21;
+
+    // mutable = 34;
+
+    // let my_array: [i32];
+    // my_array = [1,2,3,4,5];
+
+   // println!("The value of my_array is: {}", my_array[2]);
+
+    println!("The value of x is: {}", x);   
+    println!("The value of y is: {}", y);   
+    println!("The value of f is: {}", f);   
+}
